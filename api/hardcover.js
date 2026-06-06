1c3422d7a37496ff2ca4d6c236392cebf5f48c2b
DeQerrti <deqerrti@gmail.com>
2026-06-06 18:26:45 +0700

    Update hardcover.js

--- api/hardcover.js
+++ api/hardcover.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.HARDCOVER_TOKEN}`
+        "Authorization": process.env.HARDCOVER_TOKEN
       },
       body: JSON.stringify({ query: decodeURIComponent(query) })
     });
