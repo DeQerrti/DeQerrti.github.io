6e6e50d41c122203164d5ad7bed391f4939e3185
DeQerrti <deqerrti@gmail.com>
2026-08-16 12:21:51 +0700

    Add customTypePlural and customSources to settings
    Added new settings for custom type plural and sources.

--- js/theme.js
+++ js/theme.js
@@ -117,6 +117,8 @@ async function applyTheme() {
   window.SITE_CUSTOM_TAGS = settings.customTags || {};
   window.SITE_CUSTOM_TYPES = settings.customTypes || {};
   window.SITE_HIDDEN_TYPES = settings.hiddenTypes || [];
+  window.SITE_CUSTOM_TYPE_PLURAL = settings.customTypePlural || {};
+  window.SITE_CUSTOM_SOURCES = settings.customSources || {};
   window.SITE_CUSTOM_CATEGORIES = settings.customCategories || {};
   window.SITE_CATEGORY_COLORS = settings.categoryColors || {};
   window.SITE_GRADE_SCALE = settings.gradeScale || null;
